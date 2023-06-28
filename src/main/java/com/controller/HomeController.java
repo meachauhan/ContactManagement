2f94048a62cd128971c128cf62bb298724e80686
Akash Chauhan <meachauhan@gmail.com>
2023-06-28 21:33:45 +0530

    Small Changes in Signup Page

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +25,9 @@ public class HomeController {
 		m.addAttribute("title", "SignUp - Smart Contact Manager");
 		return "signup";
 	}
-	
+	@PostMapping(value="/do_register")
+	public String registerUser(@ModelAttribute("user") User user) {
+		return "signup";
+	}
 
 }
