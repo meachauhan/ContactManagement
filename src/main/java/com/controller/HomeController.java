6e3e6a23547c8d96608bf972935a5f80e5a0f394
Akash Chauhan <meachauhan@gmail.com>
2023-06-30 18:51:57 +0530

    Added Error Page

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +14,31 @@ import com.dao.UserRepository;
 import com.entity.User;
 
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController{
+	private static final String PATH = "/error";
+	public String getErrorPath() {
+		return PATH;
+	}
 	
 	@RequestMapping("/")
 	public String home(Model m) {
 		m.addAttribute("title", "Home - Smart Contact Manager");
 		return "home";
 	}
-	@RequestMapping("/signup")
+	@GetMapping("/signup")
 	public String signup(Model m) {
 		m.addAttribute("title", "SignUp - Smart Contact Manager");
 		return "signup";
 	}
-	@PostMapping(value="/do_register")
-	public String registerUser(@ModelAttribute("user") User user) {
-		return "signup";
+//	@PostMapping(value="/do_register")
+//	public String registerUser(@ModelAttribute("user") User user) {
+//		return "signup";
+//	}
+	@RequestMapping("/error")
+	public String errorPage(Model m) {
+		m.addAttribute("title", "Error-Page");
+		return "error";
+		
 	}
 
 }
